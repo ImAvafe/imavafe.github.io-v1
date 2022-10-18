8b3b49133b489c815030ca92b8647e770c29a4f0
Cyphical <65048459+Cyphical@users.noreply.github.com>
2022-10-18 17:58:10 -0400

    initial setup stuff

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'sussy-dark': '#111014',
+      }
+    },
+  },
+  plugins: [],
+  
+}
