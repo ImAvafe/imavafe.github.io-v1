f1bd63b2da963c9cdb0958091572130991320863
Cyphical <65048459+Cyphical@users.noreply.github.com>
2022-11-11 15:47:41 -0500

    discord embed

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const AVATAR_THUMB_ENDPOINT = "https://thumbnails.roproxy.com/v1/users/avatar?userIds=144146784&size=352x352&format=Png&isCircular=false";
 const AVATAR = document.getElementById("avatar");
 const FAVICON = document.querySelector("link[rel~='icon']");
+const EMBED_IMAGE = document.querySelectorAll('[property="og:image"]').item(0)
 
 function setRobloxAvatarThumb() {
     fetch(AVATAR_THUMB_ENDPOINT).then(function(response) {
         response.json().then(function(data) {
             const thumbUrl = data.data[0].imageUrl
             AVATAR.style.backgroundImage = "url("+thumbUrl+")";
+            EMBED_IMAGE.setAttribute("content", thumbUrl)
         })
     });
 }
