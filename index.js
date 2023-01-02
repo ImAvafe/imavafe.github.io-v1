e1bc81e1861660b26c3bdf3ea1db05030c3223a6
Avafe <65048459+ImAvafe@users.noreply.github.com>
2023-01-02 14:55:07 -0500

    live avatar update

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const AVATAR_THUMB_ENDPOINT = "https://thumbnails.roproxy.com/v1/users/avatar?userIds=144146784&size=352x352&format=Png&isCircular=false";
-const AVATAR = document.getElementById("avatar");
+const AVATAR_THUMB_ENDPOINT = "https://thumbnails.roproxy.com/v1/users/avatar-headshot?userIds=144146784&size=352x352&format=Png&isCircular=true";
+const AVATAR_IMG = document.getElementById("avatar-img");
 const FAVICON = document.querySelector("link[rel~='icon']");
 // const EMBED_IMAGE = document.querySelectorAll('[property="og:image"]').item(0)
 
@@ -7,7 +7,7 @@ function setRobloxAvatarThumb() {
     fetch(AVATAR_THUMB_ENDPOINT).then(function(response) {
         response.json().then(function(data) {
             const thumbUrl = data.data[0].imageUrl
-            AVATAR.style.backgroundImage = "url("+thumbUrl+")";
+            AVATAR_IMG.setAttribute('src', thumbUrl);
             // EMBED_IMAGE.setAttribute("content", thumbUrl)
         })
     });
